ee6dc24dbc6166d1aad2d0fe699dcb15fb9f21a8
Velmrok <splawis3@gmail.com>
2025-03-21 19:03:14 +0100

    .

---
+++ PokerAnalyzer/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+LoadFile();
+
+
+
+void LoadFile()
+{
+    string rootpath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+      string path = Path.Combine(rootpath, "Data", "test.txt");
+       
+        if (File.Exists(path))
+        {
+            string[] lines = File.ReadAllLines(path); 
+
+            foreach (string line in lines)  
+            {
+                Console.WriteLine(line);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Plik nie istnieje.");
+        }
+}
