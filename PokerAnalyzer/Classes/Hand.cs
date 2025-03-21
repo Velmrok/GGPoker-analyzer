4c3afc3bacf44d72cf958c79f7fa60ddef8b4a49
Velmrok <splawis3@gmail.com>
2025-03-21 19:12:58 +0100

    added hand class, added reading file function in Program.cs

--- PokerAnalyzer/Classes/Hand.cs
+++ PokerAnalyzer/Classes/Hand.cs
@@ -1,5 +1,9 @@
 public class Hand
 {
-    
+    Hand(string hand){
+        
+    }
+    public int player_count;
+
     
 }
--- PokerAnalyzer/Program.cs
+++ PokerAnalyzer/Program.cs
@@ -1,24 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-LoadFile();
+string[] file = LoadFile();
+
+for (int i = 0; i < file.Length; i++)
+{
+     if (file[i] == "") System.Console.WriteLine(file[i+1]);
+}
 
 
-
-void LoadFile()
+string[] LoadFile()
 {
     string rootpath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
-      string path = Path.Combine(rootpath, "Data", "test.txt");
-       
-        if (File.Exists(path))
-        {
-            string[] lines = File.ReadAllLines(path); 
+    string path = Path.Combine(rootpath, "Data", "test.txt");
 
-            foreach (string line in lines)  
-            {
-                Console.WriteLine(line);
-            }
-        }
-        else
-        {
-            Console.WriteLine("Plik nie istnieje.");
-        }
+    if (File.Exists(path))
+    {
+        return File.ReadAllLines(path);
+
+       
+    }
+    else
+    {
+        return null;
+    }
+       
 }
